7d82267d14ad25cbe24fcf049761db395665c0a4
Crypto Canary <ssaxton@kent.edu>
2020-04-24 00:03:50 -0400

    trivial typo correction

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2012 The Bitcoin developers
-// Copyright (c) 2014-2012 The Sterlingcoin developers
+// Copyright (c) 2014-2020 The Sterlingcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
