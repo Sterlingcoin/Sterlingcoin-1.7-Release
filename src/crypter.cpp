e58e874429e5f15251c6613538b8da1aeaa647d2
CryptoCanary <ssaxton@kent.edu>
2020-05-25 03:50:02 -0400

    Bring back opensslv1.0 support in crypter.

--- src/crypter.cpp
+++ src/crypter.cpp
@@ -39,8 +39,8 @@ bool CCrypter::SetKeyFromPassphrase(const SecureString& strKeyData, const std::v
 
     if (i != (int)WALLET_CRYPTO_KEY_SIZE)
     {
-        OPENSSL_cleanse(chKey, sizeof(chKey));
-        OPENSSL_cleanse(chIV, sizeof(chIV));
+        OPENSSL_cleanse(&chKey, sizeof chKey);
+        OPENSSL_cleanse(&chIV, sizeof chIV);
         return false;
     }
 
@@ -94,6 +94,7 @@ bool CCrypter::Encrypt(const CKeyingMaterial& vchPlaintext, std::vector<unsigned
     if (fOk) fOk = EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, chKey, chIV);
     if (fOk) fOk = EVP_EncryptUpdate(ctx, &vchCiphertext[0], &nCLen, &vchPlaintext[0], nLen);
     if (fOk) fOk = EVP_EncryptFinal_ex(ctx, (&vchCiphertext[0])+nCLen, &nFLen);
+    EVP_CIPHER_CTX_cleanup(ctx);
     EVP_CIPHER_CTX_free(ctx);
 #endif
 
@@ -117,9 +118,9 @@ bool CCrypter::Decrypt(const std::vector<unsigned char>& vchCiphertext, CKeyingM
 #if OPENSSL_VERSION_NUMBER < 0x10100000
     EVP_CIPHER_CTX ctx;
 #else
-    EVP_CIPHER_CTX *ctx;
+    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+    if (!ctx) return false;
 #endif
-    //if (!ctx) return false;
 
     bool fOk = true;
 
